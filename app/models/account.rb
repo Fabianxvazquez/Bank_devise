b353b4c364a1ab9ce000df8e905f71ff6c2c1507
Fabian Vazquez <Fabianxvazquez@gmail.com>
2020-02-25 16:10:25 -0700

    added navbr

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,3 +1,4 @@
 class Account < ApplicationRecord
+  validates :balance, numericality: { greater_than_or_equal_to: 25, message: "Must be at least $25" }
   belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root 'accounts#index'
+  get 'static_pages/index'
+  root 'static_pages#index'
   devise_for :users
   resources :accounts 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
